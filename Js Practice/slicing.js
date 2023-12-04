ac3d04817ed0a8baa82374b30eb483b40e835db3
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-04 23:28:15 +0100

    slicing

--- Js Practice/slicing.js
+++ Js Practice/slicing.js
@@ -1,6 +1,1 @@
-let myName= prompt("what is your name");
-alert("hi " + myName);
-let tweet= prompt("type your tweet here.");
-alert("You have used " + (tweet.length) + " characters. " + (140-tweet.length) + " characters to go.")
-tweetLen = tweet.slice(0,140);
-alert(tweetLen);
+alert(prompt("Type your tweet here").slice(0,140));
