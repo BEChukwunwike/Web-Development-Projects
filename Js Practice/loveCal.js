00b92a782c7a7ac44ce74d7c0164d2a77c4fd2ff
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-13 01:34:21 +0100

    love calculator

---
+++ Js Practice/loveCal.js
@@ -0,0 +1,4 @@
+let userName = prompt("Your name: ");
+let partnerName = prompt("Your partner's name: ");
+let compatibility = Math.random() * 100;
+console.log("You are " + Math.round(compatibility) + "% compatible with " + partnerName + ".");
