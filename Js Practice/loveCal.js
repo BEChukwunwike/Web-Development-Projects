c0fcd555781a1978c67411b65a1939222fbb8972
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-17 18:20:17 +0100

    99 bottles of beer

--- Js Practice/loveCal.js
+++ Js Practice/loveCal.js
@@ -6,6 +6,6 @@ if (compatibility < 50){
     console.log("You are not a good match.");
 } else if (compatibility > 50 && compatibility < 70) {
     console.log("You are a good match. Put in the work and make beautiful home");
-} else (compatibility >= 70) {
+} else {
     console.log("You are a great match. You are a power couple.");
 }
