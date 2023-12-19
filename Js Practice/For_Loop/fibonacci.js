4faa65b9104cf94c385d74fea3cbd0f4677dc13d
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-19 10:43:42 +0100

    updated fibonacci

--- Js Practice/For_Loop/fibonacci.js
+++ Js Practice/For_Loop/fibonacci.js
@@ -1,13 +1,13 @@
 function fibonacciGenerator (n) {
-        var sequence = [];
+        let sequence = [];
         //Write your code here:
-        for(var i = 0; i <= n; i++){
+        for(let i = 0; i <= n; i++){
             if (i === 0){
                 sequence.push(0)
             } else if (i == 1) {
                 sequence.push(1)
             } else {
-                var nextNum = sequence[i - 1] + sequence[i - 2];
+                let nextNum = sequence[i - 1] + sequence[i - 2];
                 sequence.push(nextNum);
             }
         }
