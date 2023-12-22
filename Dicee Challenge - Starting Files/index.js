c6f90fed32995e8e63e014b4d424c703ca7a7616
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-22 19:14:18 +0100

    Fix duplicate code in index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -6,3 +6,14 @@ const imagesPath2 = `./images/dice${randomNumber2}.png`;
 
 document.querySelector(".img1").setAttribute("src", imagesPath1);
 document.querySelector(".img2").setAttribute("src", imagesPath2);
+
+const resultElement = document.getElementById('result');
+
+// Compare dice values and update the h1 text accordingly
+if (randomNumber1 > randomNumber2) {
+    resultElement.textContent = 'Player 1 Wins!';
+} else if (randomNumber2 > randomNumber1) {
+    resultElement.textContent = 'Player 2 Wins!';
+} else {
+    resultElement.textContent = 'It\'s a Draw!';
+}
