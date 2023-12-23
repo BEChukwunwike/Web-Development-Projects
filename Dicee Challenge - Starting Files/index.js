c78df3d103cfb7d77af6e028bd5bf260e05044de
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-23 10:12:36 +0100

    Refactored dice image paths

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,8 +1,13 @@
 randomNumber1 = Math.round((Math.random() * 5)+ 1);
 randomNumber2 = Math.round((Math.random() * 5)+ 1);
 
-const imagesPath1 = `./images/dice${randomNumber1}.png`;
-const imagesPath2 = `./images/dice${randomNumber2}.png`;
+const imagesPath1 = "./images/dice" + randomNumber1 + ".png"
+const imagesPath2 = "./images/dice" + randomNumber2 + ".png"
+
+
+
+// const imagesPath1 = `./images/dice${randomNumber1}.png`;
+// const imagesPath2 = `./images/dice${randomNumber2}.png`;
 
 document.querySelector(".img1").setAttribute("src", imagesPath1);
 document.querySelector(".img2").setAttribute("src", imagesPath2);
