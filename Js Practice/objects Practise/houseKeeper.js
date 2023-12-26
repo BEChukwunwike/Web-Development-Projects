47c0ef2a1b5ed019ef6152b9fc4ac81a81f7f6ae
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-26 21:57:11 +0100

    Refactor variable names for clarity and consistency
    This commit refactors variable names in the codebase to improve clarity and maintain consistency. The changes help to enhance code readability and make it easier for future developers to understand and maintain the code.

---
+++ Js Practice/objects Practise/houseKeeper.js
@@ -0,0 +1,7 @@
+function HouseKeeper(yearsofExperience, name, cleaningRepertoire){
+    this.yearsofExperience = yearsofExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+
+var houseKeeper1 = new HouseKeeper(12, "Jane", ["bathroom", "lobby", "bedroom"]);
