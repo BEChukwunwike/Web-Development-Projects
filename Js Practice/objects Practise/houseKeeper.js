02221a31043b167f211d30dc5eae4d90a87b0087
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-27 23:20:19 +0100

    Add clean method to HouseKeeper class

--- Js Practice/objects Practise/houseKeeper.js
+++ Js Practice/objects Practise/houseKeeper.js
@@ -2,10 +2,13 @@ function HouseKeeper(yearsofExperience, name, cleaningRepertoire){
     this.yearsofExperience = yearsofExperience;
     this.name = name;
     this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function(){
+        console.log("Cleaning in progress");
+    }
 }
 
 let houseKeeper1 = new HouseKeeper(12, "Jane", ["bathroom", "lobby", "bedroom"]);
-console.log(houseKeeper1);
+console.log(houseKeeper1.clean());
 
 let houseKeeper2 = new HouseKeeper(5, "Grace", ["bedroom", "kitchen", "toilets"])
 console.log(houseKeeper2.name);
