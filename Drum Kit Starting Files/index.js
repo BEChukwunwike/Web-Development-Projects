e372ea835ffa2360f60134c4e04a179064c9e7a4
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-29 23:02:57 +0100

    Add button animation for button click and keyboard press

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,14 +1,19 @@
+//For button clicked
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
 
     let buttonInnerHtml = this.innerHTML;
     playSound(buttonInnerHtml);
+    animateButton(buttonInnerHtml);
     });
 }
+
+//For keyboard pressed
 
     document.addEventListener("keydown", function(event){
         let keyPressed = event.key;
         playSound(keyPressed);
+        animateButton(keyPressed);
     })
 
     function playSound(key){
@@ -46,3 +51,10 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
         }
     }
 
+    function animateButton(keyClicked) {
+        let activeButton = document.querySelector("." + keyClicked);
+        activeButton.classList.add("pressed");
+        setTimeout(function(){
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
