ab8d4733b5e0a7f245b9003b4832c2eef0d76383
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-01 23:17:05 +0100

    Add random color selection and play sound upon click

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,24 @@
-function nextSequence(){
-    let randomNumber = Math.round(Math.random()*3);
-    let buttonColours = ["green", "red", "yellow", "blue"];
-    let randomChosenColour = buttonColours[randomNumber];
-    let randomChosenColourId = $("#" + randomChosenColour);
-    $(randomChosenColourId).fadeOut(100).fadeIn(100);
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-    gamePattern.push(randomChosenColour);
-    level++;
-    $("#level-title").text("Level " + level);
+let gamePattern =[];
 
+
+function nextSequence(){
+    //random numbers from 0-3
+    let randomNumber = Math.round(Math.random()*3);
+    //array of colours
+    let buttonColours = ["green", "red", "yellow", "blue"];
+    //chooses random colour
+    let randomChosenColour = buttonColours[randomNumber];
+    //Chooses the id
+    let randomChosenColourId = "#" + randomChosenColour;
+    //animate upon click
+    $(randomChosenColourId).fadeToggle(100);
+    //play sound
+    playSound(randomChosenColour)
+    
+    gamePattern.push(randomChosenColour);
+}
+
+function playSound(colour){
+    let audio = new Audio("sounds/" + colour + ".mp3");
+    audio.play();
 }
