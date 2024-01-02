e106b0f5a5d82c070afc30576f928458f0b32bb1
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-03 00:10:35 +0100

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,6 @@
 let gamePattern =[];
-
+let userClickedPattern = [];
+let gameStarted = false;
 
 function nextSequence(){
     //random numbers from 0-3
@@ -11,7 +12,7 @@ function nextSequence(){
     //Chooses the id
     let randomChosenColourId = "#" + randomChosenColour;
     //animate upon click
-    $(randomChosenColourId).fadeToggle(100);
+    $(randomChosenColourId).fadeIn(100).fadeOut(100).fadeIn(100);
     //play sound
     playSound(randomChosenColour)
     
@@ -21,4 +22,67 @@ function nextSequence(){
 function playSound(colour){
     let audio = new Audio("sounds/" + colour + ".mp3");
     audio.play();
+}
+
+//start game
+$(document).on("keydown", function() {
+    if (!gameStarted) {
+        startGame();
+    }
+    });
+//Function to start game    
+function startGame(){
+    nextSequence();
+    gameStarted = true;
+    }  
+
+  //on key press, add color to array
+$(".btn").on("click", function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+  });
+
+//animate button press
+function animatePress(colour) {
+    $("#" + colour).addClass("pressed");
+    setTimeout(function() {
+      $("#" + colour).removeClass("pressed");
+    }, 100);
+  }  
+//check if users answer is correct or not
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                userClickedPattern = [];
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        gameOver();
+    }
+}
+
+//game over
+function gameOver() {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    gamePattern = [];
+    userClickedPattern = [];
+    gameStarted = false;
+    $("#level-title").text("Game Over! Press A Key to Restart");
+    $(document).on("keydown", function() {
+        restartGame();
+      });
+  }
+  function restartGame() {
+    $("#level-title").text("Press A Key to Start");
+    $(document).off("keydown"); // remove previous keydown event listener
+    startGame(); // start the game again
 }
