5c2ec444629831ded28515a56d3171d7e6a66bf4
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-12 22:34:21 +0100

    Update response message in app.get() method

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res)=> {
-    res.send("Hello User");
+    res.send("<h1>Hello World</h1>");
 })
 
 app.listen(3000, () =>{
