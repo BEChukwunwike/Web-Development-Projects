e6951b1c6e6875255a3e53ace6eb075019c5168f
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-12 22:40:14 +0100

    Add about route to index.js

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res)=> {
     res.send("<h1>Hello World</h1>");
 })
+app.get("about",(req, res) => {
+    res.send("<h1>About Us</h1>");
+})
 
 app.listen(3000, () =>{
     console.log(`You have started a server on port ${3000}`);
