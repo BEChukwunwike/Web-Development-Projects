2650778c6af4aadac2e872c43f69272bce173570
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-12 22:44:06 +0100

    Add contact route

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -7,8 +7,9 @@ app.get("/", (req, res)=> {
 app.get("about",(req, res) => {
     res.send("<h1>About Us</h1>");
 })
-
-
+app.get("contact",(req, res) =>{
+    res.send("<h1>Contact Us</h1>");
+})
 app.listen(3000, () =>{
     console.log(`You have started a server on port ${3000}`);
 })
