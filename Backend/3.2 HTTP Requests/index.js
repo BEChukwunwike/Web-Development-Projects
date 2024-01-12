60ccf9a54fac7fce353aa196a5ddba4d501a18d3
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-12 22:31:03 +0100

    Add route for root endpoint

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res)=> {
+    res.send("Hello User");
+})
+
 app.listen(3000, () =>{
     console.log(`You have started a server on port ${3000}`);
 })
