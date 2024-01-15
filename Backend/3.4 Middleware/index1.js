350e6a173425dafbaad867ca502cd8cfe4ea9013
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-15 17:58:39 +0100

    updated

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -8,7 +8,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get("/", (req, res) => {
